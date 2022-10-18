0f28da003536aadfa8208951fed40b79ba2a077f
LuisHernandezCoding <LuisHernandezCoding@gmail.com>
2022-10-17 21:23:00 -0500

    - fix logic_spec.rb

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/logic'
 describe 'logic' do
   logic = Logic.new
   it 'should take a word from the dictionary and return it' do
-    word = logic.get_word
+    word = logic.getting_word
     expect(word).to be_a(String)
     expect(word.length).to be_between(5, 12).inclusive
   end

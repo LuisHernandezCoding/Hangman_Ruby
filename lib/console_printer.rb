54b8d5d1a5b807ba27e2694a27faeee6204fde70
LuisHernandezCoding <LuisHernandezCoding@gmail.com>
2022-10-18 05:11:32 -0500

    - Updated display to show a real hangman - Minor change on console_printer.rb

--- lib/console_printer.rb
+++ lib/console_printer.rb
@@ -1,7 +1,7 @@
 module ConsolePrinter
-  def print_message(messages, full_size, bg_color = 'bg_blue', inner_bg = 'bg_cyan', use_symbol: false)
-    frame = getting_frame('+', use_symbol, full_size, inner_bg)
-    symbol = getting_symbol('+', use_symbol, inner_bg)
+  def print_message(messages, full_size, bg_color = 'bg_blue', inner_bg = 'bg_cyan', use_frame: false)
+    frame = getting_frame('+', use_frame, full_size, inner_bg)
+    symbol = getting_symbol('+', use_frame, inner_bg)
     margin = ' ' * ((full_size / 20) + 1)
     puts margin + (' ' * full_size).send(bg_color)
     puts margin + '           '.send(bg_color) + frame + '           '.send(bg_color)
@@ -15,16 +15,16 @@ module ConsolePrinter
 
   private
 
-  def getting_frame(symbol, use_symbol, full_size, color = 'bg_black')
-    if use_symbol
+  def getting_frame(symbol, use_frame, full_size, color = 'bg_black')
+    if use_frame
       (symbol * (full_size - 22)).send(color)
     else
       (' ' * (full_size - 22)).send(color)
     end
   end
 
-  def getting_symbol(symbol, use_symbol, color = 'bg_black')
-    if use_symbol
+  def getting_symbol(symbol, use_frame, color = 'bg_black')
+    if use_frame
       (symbol * 2).send(color)
     else
       '  '.send(color)
--- lib/display.rb
+++ lib/display.rb
@@ -10,7 +10,7 @@ class Display
     @word = word
     @word_values = '_ ' * word.length
     @show_instructions = false
-    @cheat = false
+    @cheat = true
   end
 
   def update_values(guesses)
@@ -19,15 +19,49 @@ class Display
     end.join(' ')
   end
 
-  def update( message, guesses, guesses_left, word)
+  def update(message, guesses, guesses_left, word)
     system('clear') or system('cls')
-    print_message(['Hangman Game', 'By: @LuisHernandezCoding'], 100, 'bg_white', 'bg_black', use_symbol: true)
-    print_message([message, "Guesses left: #{guesses_left}"], 100, 'bg_dark gray', 'bg_black')
-
     print_instructions if @show_instructions
-    print_args = ["Guesses: #{guesses.join(', ')}", @word_values]
-    print_args[2] = @cheat ? "CHEATING: #{word}" : nil
-    print_message(print_args, 100, 'bg_black', 'bg_red', use_symbol: true)
+
+    print_args = getting_animation
+    print_args.push(@cheat ? "CHEATING: #{word}" : nil)
+    print_args = print_args.map.with_index do |arg, index|
+      case index
+      when 0
+        print_args[0] += '  _  _                '.center(30)
+      when 1
+        print_args[1] += ' | || |__ _ _ _  __ _ '.center(30)
+      when 2
+        print_args[2] += ' | __ / _` | \' \/ _` |'.center(30)
+      when 3
+        print_args[3] += ' |_||_\__,_|_||_\__, |'.center(30)
+      when 4
+        print_args[4] += ' |  \/  |__ _ _ |___/ '.center(30)
+      when 5
+        print_args[5] += ' | |\/| / _` | \' \    '.center(30)
+      when 6
+        print_args[6] += ' |_|  |_\__,_|_||_|   '.center(30)
+      when 7
+        print_args[7] += 'by: @LuisHernandezCoding'.center(30)
+      when 9
+        print_args[9] += 'Input a letter to guess'.center(30)
+      when 11
+        print_args[11] += 'Guesses left:'.center(30)
+      when 12
+        print_args[12] += guesses_left.to_s.center(30)
+      when 14
+        print_args[14] += 'Word:'.center(30)
+      when 15
+        print_args[15] += @word_values.center(30)
+      when 17
+        print_args[17] += 'Your Guesses:'.center(30)
+      when 18
+        print_args[18] += guesses.join(', ').center(30)
+      else
+        print_args[index] = (arg.to_s + (' ' * 30)) if arg
+      end
+    end
+    print_message(print_args, 100, 'bg_black', 'bg_red', use_frame: true)
   end
 
   def print_instructions
@@ -42,5 +76,59 @@ class Display
     instructions_text[7] = 'If you run out of guesses before you guess the word, you lose!'
     instructions_text[8] = 'Good luck!'
     print_message(instructions_text, 100, 'bg_green')
+  end
+
+  def getting_animation(step = 0)
+    animation_args = []
+    case step
+    when 0
+      animation_args << '    ╔════════════()══════╗       '
+      animation_args << '    ║ ╔══════════()══════╝       '
+      animation_args << '    ║ ║  / /     ()              '
+      animation_args << '    ║ ║ / /      () ,~~~.        '
+      animation_args << '    ║ ║/ /       ()/  _  \\       '
+      animation_args << '    ║ ║ /        ()G  °  _\\      '
+      animation_args << '    ║ ║/         ()\````  /      '
+      animation_args << '    ║ ║          () ```__/       '
+      animation_args << '    ║ ║          /Y . . Y\\       '
+      animation_args << '    ║ ║         // ║   ║ \\\\      '
+      animation_args << '    ║ ║        //  ║ . ║  \\\\     '
+      animation_args << '    ║ ║       \')   ║   ║   (`    '
+      animation_args << '    ║ ║            ║║\'║║         '
+      animation_args << '    ║ ║            ║║ ║║         '
+      animation_args << '    ║ ║            ║║ ║║         '
+      animation_args << '    ║ ║            ║║ ║║         '
+      animation_args << '    ║ ║           / ║ ║ \\        '
+      animation_args << '  """"""""""""""""║══════════║"""║/% '
+      animation_args << '  """"║"║"""""""""║══════════║"║"║/   '
+      animation_args << '    ║ ║                     ║ ║  '
+      animation_args << '    : :                     : :  '
+      animation_args << '    . .                     . .  '
+    when 12
+      animation_args << '    ╔════════════()══════╗       '
+      animation_args << '    ║ ╔══════════()══════╝       '
+      animation_args << '    ║ ║  / /     ()              '
+      animation_args << '    ║ ║ / /      () ,~~~.        '
+      animation_args << '    ║ ║/ /       ()/  _  \\       '
+      animation_args << '    ║ ║ /        ()G  x  _\\      '
+      animation_args << '    ║ ║/         ()\````/_/      '
+      animation_args << '    ║ ║          () ```__/       '
+      animation_args << '    ║ ║          /Y . . Y\\       '
+      animation_args << '    ║ ║         // ║   ║ \\\\      '
+      animation_args << '    ║ ║        //  ║ . ║  \\\\     '
+      animation_args << '    ║ ║       \')   ║   ║   (`    '
+      animation_args << '    ║ ║            ║║\'║║         '
+      animation_args << '    ║ ║            ║║ ║║         '
+      animation_args << '    ║ ║            ║║ ║║         '
+      animation_args << '    ║ ║            ║║ ║║         '
+      animation_args << '    ║ ║           / ║ ║ \\        '
+      animation_args << '""""""""""""""""║_`-\' `-\' .║"""║  '
+      animation_args << '""""║"║"""""""""\ \       \'║"║"║\ '
+      animation_args << '    ║ ║          \ \        ║ ║\% '
+      animation_args << '    : :           \ \       : :  '
+      animation_args << '    . .            \'\'       . .  '
+    end
+    animation_args.each { |line| line.center(20) }
+    animation_args
   end
 end
